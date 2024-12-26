77c40d491c0ba779a3faa5a19777f815544eba13
TAHPAPANGKORN <thtahzach@gmail.com>
2024-12-26 19:32:26 +0700

    fix[script.js style.css]: button move bug

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const dodgeButton = document.getElementById("dodge-button");
+    const linkButton = document.getElementById("link-button");
 
     dodgeButton.addEventListener("mouseover", (event) => {
         const rect = dodgeButton.getBoundingClientRect();
@@ -7,15 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const offsetY = event.clientY - rect.top;
 
         // Generate random movement distances
-        const moveX = (Math.random() > 0.5 ? 1 : -1) * (Math.random() * 300);
+        const moveX = (Math.random() > 0.5 ? 1 : -1) * (Math.random() * 500);
         const moveY = (Math.random() > 0.5 ? 1 : -1) * (Math.random() * 200);
 
         dodgeButton.style.transform = `translate(${moveX}px, ${moveY}px)`;
+        dodgeButton.style.cursor = "default";
+        linkButton.style.cursor = "default";
     });
 
-    dodgeButton.addEventListener("mouseout", () => {
-        setTimeout(() => {
-            dodgeButton.style.transform = 'translate(0, 0)';
-        }, 500); // 1-second delay
-    });
+    // dodgeButton.addEventListener("mouseout", () => {
+    //     setTimeout(() => {
+    //         dodgeButton.style.transform = 'translate(0, 0)';
+    //     }, 10000); // 1-second delay
+    // });
 });
